c8d6d160ec69efc9492f634e51329abc8e147288
TheOmegaForReal <81127174+BoujbiriMahmoud@users.noreply.github.com>
2023-03-12 20:44:25 +0100

    Update dnscan.py

--- tools/dnscan.py
+++ tools/dnscan.py
@@ -1,7 +1,8 @@
 import requests
 
 
-def dnscan(domain, filename):
+def dnscan(domain, filename_suffix):
     response = requests.get(f"https://networkcalc.com/api/dns/lookup/{domain}").text
-    with open(filename, 'w') as f:
+    with open(f"dnscan_{filename_suffix}.txt", 'w') as f:
         f.write(response)
+    print(f"Le fichier dnscan_{filename_suffix}.txt a été créé avec succès.")
