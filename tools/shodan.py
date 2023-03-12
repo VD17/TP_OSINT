44ea6f1f23ebeae02f1897b1f62683c82c224224
TheOmegaForReal <81127174+BoujbiriMahmoud@users.noreply.github.com>
2023-03-12 22:04:21 +0100

    Update shodan.py

--- tools/shodan.py
+++ tools/shodan.py
@@ -4,14 +4,14 @@ from pprint import pprint
 import requests
 from shodan import Shodan
 
-def shodan_search(domain, api_key, filename_suffix):
+def shodan_search(domain, api_key, filename_suffix): # Récupère l'api ainsi que le host
     api = Shodan(api_key)
     result = api.host(domain)
     
-    with open(f"shodan_{filename_suffix}", 'w') as f:
+    with open(f"shodan_{filename_suffix}", 'w') as f: # permet de nommer le fichier de résultat
         json.dump(result, f, indent=4)
     
-    org = result['org']
+    org = result['org']   # ces lignes permettent d'afficher le résultat pertinants sur la console, le résultat complet est affiché dans le fichier .txt
     dom = result['domains']
     ip_str = result['ip_str']
     ports = result['ports']
