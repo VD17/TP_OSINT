931cf0be1de7323de248259d7d5070f438128f2f
TheOmegaForReal <81127174+BoujbiriMahmoud@users.noreply.github.com>
2023-03-12 22:05:15 +0100

    Update theharvester.py

--- tools/theharvester.py
+++ tools/theharvester.py
@@ -1,12 +1,14 @@
 import subprocess
 
-def run_theharvester(domain, navigator):
+def run_theharvester(domain, navigator): # récupere le nom de domaine et le navigateur entré par l'utilisateur.
     cmd = ['theHarvester', '-d', domain, '-l', '200', '-b', navigator]
-    result = subprocess.run(cmd, capture_output=True, text=True)
+    result = subprocess.run(cmd, capture_output=True, text=True) 
+
     return result.stdout 
 
 if __name__ == '__main__':
 
-    output = run_theharvester(domain, navigator)
-    print(output)
 
+
+    output = run_theharvester(domain, navigator) 
+    print(output)
