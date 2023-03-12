ac0fc2b707571c6ea35b5ca1974a54c2cf7c7e02
TheOmegaForReal <81127174+BoujbiriMahmoud@users.noreply.github.com>
2023-03-12 21:48:14 +0100

    Update theharvester.py

--- tools/theharvester.py
+++ tools/theharvester.py
@@ -3,7 +3,7 @@ import subprocess
 def run_theharvester(domain, navigator):
     cmd = ['theHarvester', '-d', domain, '-l', '200', '-b', navigator]
     result = subprocess.run(cmd, capture_output=True, text=True)
-    return result.stdout
+    return result.stdout 
 
 if __name__ == '__main__':
 
